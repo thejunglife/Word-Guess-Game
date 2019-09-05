1d0e2082f94a16d1777c68df514b604116969c54
Paul <thejunglife@gmail.com>
2019-09-05 12:52:34 -0700

    built functional app need to css still

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,74 @@
 // Number of Guesses Remaning
 
 // list of words to be used
-let gameWords = ['Tyrion Lannister', 'Sansa Stark', 'Jon Snow', 'Daenerys Targaryen', 'Cersei Lannister']
+let gameWords = ['TyrionLannister', 'SansaStark', 'JonSnow', 'DaenerysTargaryen', 'CerseiLannister']
+
 // will randomize words 
-let randomWord = Math.floor(Math.random() * gameWords.length);
-let wordChosen = gameWords[randomWord]
-let rightWord = [];
-let wrongGuess= [];
-// test
-console.log(wordChosen)
+const randomWord =  function () {
+return gameWords[Math.floor(Math.random() * gameWords.length)]
+ .toLowerCase()
+};
 
-let totalWins = 0;
-let currentWord = [];
-let lettersGuessed = [];
-let choicesLeft = 10;
-let underScore = [];
+let wins = 0;
+let losses = 0;
+let guesses = 10;
+let lettersGuessed = []
+let word = randomWord ()
 
-// get user input and start game
-document.addEventListener('keypress', (event) => {
-  let keyword = String.fromCharCode(event.keyCode);
+const reset = function () {
+  word = randomWord()
+  lettersGuessed = []
+  guesses = 10
+  displayWord()
+  document.getElementById('wins').textContent = 'WINS: ' + wins
+  document.getElementById('losses').textContent = 'LOSSES: ' + losses
+  lettersGuessed.join(', ')
+  document.getElementById('guessesLeft').textContent = 'Guesses Left: ' + guesses
+
+
+}
+
+const displayWord = function (chosen) {
+  let underScore = ''
+  let winStatus = true
+word.split('').forEach(function (letter) {
+  if (letter === chosen || lettersGuessed.indexOf (letter) 
+  !== -1) {
+    
+    underScore += `${letter} `
+  } else {
+    underScore += '_ '
+    winStatus = false 
+  }
+})
+if (winStatus) {
+  alert(`You Won! The Word Was: ${word}`)
+  wins++
+  reset()
+}
+document.getElementById('currentWord').textContent = underScore
+}
+
+document.onkeyup = function (event) {
+  if (event.keyCode >= 65 && event.keyCode <= 90) {
+    if (lettersGuessed.indexOf(event.key) === -1) {
+      lettersGuessed.push(event.key)
+      document.getElementById('lettersGuessed').textContent = lettersGuessed.join(', ')
+    if (word.includes(event.key)) {
+      displayWord()
+    } else {
+      guesses--
+      document.getElementById('guessesLeft').textContent = 'Guesses Left: ' + guesses
+      if (guesses <= 0)  {
+        alert(`You Lost! The Word Was: ${word}`)
+        losses++
+        reset()
+        
+      }
+    }
+  }
+}
+}
+
+
+displayWord()
